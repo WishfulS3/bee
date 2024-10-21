4459ead7a1d59d692292ff3ae8e2835b84bd23eb
TOD-nights <xu1271669848@gmail.com>
2024-10-21 15:18:54 +0200

    1
    1

--- config.js
+++ config.js
@@ -3,5 +3,5 @@ module.exports = {
   note: '商品分类支持区分门店',
   subDomain: "98coffee", // 根据教程 https://www.it120.cc/help/qr6l4m.html 查看并设置你自己的 subDomain
   merchantId: 67821, // 商户ID，可在后台工厂设置-->商户信息查看
-  customerServiceType: 'QW' // 客服类型，QW为企业微信，需要在后台系统参数配置企业ID和客服URL，否则为小程序的默认客服
+  customerServiceType: 'QW' // 客服类型，QW为企业微信，需要在后台系统参数配置企业ID和客服URL，否则为小程序的默认客服o
 }
